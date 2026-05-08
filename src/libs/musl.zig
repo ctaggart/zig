b4a571413e744c0c02eaee696d7e29856ad4fd54
ctaggart <ctaggart@users.noreply.github.com>
2026-05-08 11:11:24 +0000

    libzigc: migrate pthread_cancel to Zig

--- src/libs/musl.zig
+++ src/libs/musl.zig
@@ -866,7 +866,7 @@ const src_files = [_][]const u8{
     "musl/src/stdio/putc_unlocked.c",
     "musl/src/stdio/putchar.c",
     "musl/src/stdio/putchar_unlocked.c",
-    "musl/src/thread/pthread_cancel.c",
+    //"musl/src/thread/pthread_cancel.c", // migrated to lib/c/thread.zig
     //"musl/src/thread/pthread_cond_timedwait.c", // migrated to lib/c/thread.zig
     "musl/src/thread/pthread_create.c",
     "musl/src/thread/pthread_join.c",
